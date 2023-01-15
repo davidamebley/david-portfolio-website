fd68a3e1c8926015ad4f185536458ce4ebb7c6d9
David Amebley <davameb@gmail.com>
2023-01-15 20:31:54 +0200

    Add schemas

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonials from "./testimonials";
+import abouts from "./abouts";
+import brands from "./brands";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import works from "./works";
+
+export const schemaTypes = [
+    testimonials,
+    abouts,
+    brands,
+    contact,
+    experiences,
+    skills,
+    workExperience,
+    works,
+]
