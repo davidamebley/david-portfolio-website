1112b79342951230a575a5e7f5fb4e847ee0cc46
David Amebley <davameb@gmail.com>
2023-01-31 16:09:40 +0200

    Add About profiles

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -2,6 +2,14 @@ import React, {useEffect, useState} from 'react';
 import { motion } from 'framer-motion';
 
 import './About.scss';
+import { images } from '../../constants'
+
+const abouts = [
+  {title: "Frontend Developer", description: "I design frontend apps for the web", imgUrl: images.about01},
+  {title: "Backend Developer", description: "I create the backend of web applications", imgUrl: images.about02},
+  {title: "Full Stack Developer", description: "I combine the beauty of the front end to the server side to create a fully functional full-stack web app.", imgUrl: images.about03},
+  {title: "Mobile App Developer", description: "I design user-friendly mobile applications for Android and iOS", imgUrl: images.about04},
+];
 
 const About = () => {
   return (
@@ -15,7 +23,22 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        
+        {
+          abouts.map((about, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="app__profile-item"
+              key={about.title + index}
+            >
+              <img src={about.imgUrl} alt={about.title} />
+
+              <h2 className='bold-text' style={{ marginTop:20 }}>{about.title}</h2>
+              <h2 className='p-text' style={{ marginTop:10 }}>{about.description}</h2>
+            </motion.div>
+          ))
+        }
       </div>
     </>
   )
