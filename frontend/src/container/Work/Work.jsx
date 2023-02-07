8690b3e56d0baab44d34abd7aabfe4a923888175
David Amebley <davameb@gmail.com>
2023-02-07 17:32:03 +0200

    Set up initial Work Section

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {AiFillEye, AiFillGithub} from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">
+        A view of my <span>Portfolio</span>
+      </h2>
+
+      <div className="app__work-filter">
+        {['Web App', 'Mobile App', 'Backend', 'Frontend', 'PHP', 'ReactJS', 'Laravel', 'All']}
+      </div>
+    </>
   )
 }
 
