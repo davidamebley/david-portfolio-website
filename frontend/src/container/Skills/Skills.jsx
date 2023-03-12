6ebb71eb096e74c307aaa8d3da0d6931660588fa
David Amebley <davameb@gmail.com>
2023-03-12 14:38:22 +0200

    Add work experiences

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import { Tooltip as ReactTooltip }  from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -43,6 +43,45 @@ const Skills = () => {
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
               <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+
+        <motion.div className='app__skills-exp'>
+          {experience?.map((experience, index) => (
+            <motion.div
+              className='app__skills-exp-item'
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year" key={index}>
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div
+                className='app__skills-exp-works'
+              >
+                {experience.works.map((work, index) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text" id={work.name+index}>{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      anchorId={work.name+index}
+                      arrowColor="#fff"
+                      effect="solid"
+                      className="skills-tooltip"
+                      content={work.desc}
+                    />
+                  </>
+                ))}
+              </motion.div>
             </motion.div>
           ))}
         </motion.div>
