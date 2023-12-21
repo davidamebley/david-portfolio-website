fc744cfe908730ca7fd13dce02bf458865f9c8d0
David Amebley <davameb@gmail.com>
2023-12-21 03:52:27 +0200

    Added aria-label to anchor tag for accessibility in NavigationDots.js

--- frontend/src/components/NavigationDots.js
+++ frontend/src/components/NavigationDots.js
@@ -4,14 +4,15 @@ import React from 'react';
 const NavigationDots = ({ active }) => {
   return (
     <div className='app__navigation'>
-        {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
-            <a 
-            href={`#${item}`}
-            key={item + index}
-            className='app__navigation-dot'
-            style={active === item ? {backgroundColor: '#313BAC'}: { }}
-            />
-        ))}
+      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className='app__navigation-dot'
+          style={active === item ? { backgroundColor: '#313BAC' } : {}}
+          aria-label={item} // Added aria-label for accessibility
+        />
+      ))}
     </div>
   )
 }
