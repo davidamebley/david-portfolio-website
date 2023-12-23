88b648570ec1f46ee4ce05cbaa11873488e202cb
David Amebley <davameb@gmail.com>
2023-12-23 18:10:02 +0200

    Added a few more images mapped in Header.jsx

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className='app__header-circles'
       >
-        {[images.typescript, images.python, images.react].map((circle, index) => (
+        {[images.typescript, images.python, images.react, images.javascript, images.amazon].map((circle, index) => (
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
